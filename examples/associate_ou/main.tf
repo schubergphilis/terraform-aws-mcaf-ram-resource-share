3c5e885acf60c45ca26ac4b57b3d75a4a72bfe21
Keith Segovia <ksegovia@schubergphilis.com>
2025-08-06 15:21:49 +0200

    terraform fmt

--- examples/associate_ou/main.tf
+++ examples/associate_ou/main.tf
@@ -3,7 +3,7 @@ module "ram_resource_share" {
 
   name = "ram-resource-share"
   shared_resource_arns = {
-   route53-profile-1 = "arn:aws:route53profiles:eu-central-1:123456789012:profile/rp-12345ab6c7de8fab"
+    route53-profile-1 = "arn:aws:route53profiles:eu-central-1:123456789012:profile/rp-12345ab6c7de8fab"
   }
   principals = {
     dev_ou     = "arn:aws:organizations::123456789014:ou/o-a100abfooo/ou-00a1-abc1d2e3",
--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -3,6 +3,6 @@ module "ram_resource_share" {
 
   name = "ram-resource-share"
   shared_resource_arns = {
-   route53-profile-1 = "arn:aws:route53profiles:eu-central-1:123456789012:profile/rp-12345ab6c7de8fab"
+    route53-profile-1 = "arn:aws:route53profiles:eu-central-1:123456789012:profile/rp-12345ab6c7de8fab"
   }
 }

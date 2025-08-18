488fa3f4bda0b5a5379d1f67f3befc45721785f8
Keith Segovia <ksegovia@schubergphilis.com>
2025-08-18 14:05:09 +0200

    feat!: release v1.0.0

--- examples/associate_ou/main.tf
+++ examples/associate_ou/main.tf
@@ -1,6 +1,5 @@
 module "ram_resource_share" {
-  source  = "schubergphilis/mcaf-ram-resource-share/aws"
-  version = "~> 1.0.0"
+  source = "../../"
 
   name = "ram-resource-share"
   shared_resource_arns = {
